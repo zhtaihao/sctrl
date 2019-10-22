176c357f259e6d96b260184331f464d9e06faba1
zhtaihao <tadamount2@googlemail.com>
2019-10-22 17:06:21 +0200

    constructor for EditTokens

--- app/src/main/java/de/taihao/sctrl/EditTokens.java
+++ app/src/main/java/de/taihao/sctrl/EditTokens.java
@@ -1,5 +1,23 @@
 package de.taihao.sctrl;
 
-public class EditTokens {
-    
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.app.Activity;
+
+import java.util.Map;
+
+
+public class EditTokens extends Activity {
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
+    public EditTokens(Map<String, String> tokens) {
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        for (Map.Entry<String, String> token : tokens.entrySet()) {
+            editor.putString(token.getKey(), token.getValue());
+        }
+        editor.putString();
+    }
+
+
 }
