2775796046103825653859a9069228f387d4c6f7
louisPouliot <louis.pouliotutlook.de>
2019-10-22 14:54:46 +0200

    added StartServerTask

---
+++ app/src/main/java/de/taihao/sctrl/StartServerTask.java
@@ -0,0 +1,24 @@
+package de.taihao.sctrl;
+
+import android.os.AsyncTask;
+
+import net.jstorch.mc.cloudcontrol.ServerControl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartServerTask extends AsyncTask<String, Void, ServerControl > {
+
+    List<String> statusMSG = new ArrayList<>();
+
+    @Override
+    protected ServerControl doInBackground(String[] serverType) {
+        ServerControl serverControl = new ServerControl(serverType[0], statusMSG);
+        return  serverControl;
+    }
+
+    @Override
+    protected void onPostExecute(ServerControl serverControl) {
+        serverControl.startServer(statusMSG);
+    }
+}
